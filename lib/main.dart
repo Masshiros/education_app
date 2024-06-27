725b8509662797dfff2d6f2221adbd658f226341
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-06-27 13:32:34 +0700

    Feat(asset): set up app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:education_app/core/global/colours.dart';
+import 'package:education_app/core/global/fonts.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,11 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      )
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: Fonts.poppins,
+          appBarTheme: const AppBarTheme(color: Colors.transparent),
+          colorScheme:
+              ColorScheme.fromSwatch(accentColor: Colours.primaryColour),
+          useMaterial3: true,
+        ));
   }
 }
