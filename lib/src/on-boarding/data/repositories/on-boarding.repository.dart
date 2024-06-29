c83691088846dbb566612cf04d3f62c83a8ba6a8
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-06-29 10:52:14 +0700

    Feat(on-boarding): Finish on-boarding repo. All tests passed

--- lib/src/on-boarding/data/repositories/on-boarding.repository.dart
+++ lib/src/on-boarding/data/repositories/on-boarding.repository.dart
@@ -1,4 +1,6 @@
 import 'package:dartz/dartz.dart';
+import 'package:education_app/core/errors/exceptions.dart';
+import 'package:education_app/core/errors/failures.dart';
 import 'package:education_app/core/utils/typedefs.dart';
 import 'package:education_app/src/on-boarding/data/datasources/on-boarding-local-data-source.dart';
 import 'package:education_app/src/on-boarding/domain/repositories/on-boarding.repository.dart';
@@ -8,12 +10,20 @@ class OnBoardingRepository implements IOnBoardingRepository {
   final OnBoardingLocalDataSource _dataSource;
   @override
   ResultFuture<void> cacheFirstTimer() async {
-   
+    try {
+      await _dataSource.cacheFirstTimer();
+      return const Right(null);
+    } on CacheException catch (e) {
+      return Left(CacheFailure(message: e.message, statusCode: e.statusCode));
+    }
   }
 
   @override
-  ResultFuture<bool> checkIfUserIsFirstTimer() {
-    // TODO: implement checkIfUserIsFirstTimer
-    throw UnimplementedError();
+  ResultFuture<bool> checkIfUserIsFirstTimer() async {
+    try {
+      return Right(await _dataSource.checkIfUserIsFirstTimer());
+    } on CacheException catch (e) {
+      return Left(CacheFailure(message: e.message, statusCode: e.statusCode));
+    }
   }
 }
