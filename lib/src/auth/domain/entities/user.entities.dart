5a9c56098233cf1e687e4629e908fe62af86ed7c
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-07-02 08:11:20 +0700

    Feat(user): Add user entities in domain layer

---
+++ lib/src/auth/domain/entities/user.entities.dart
@@ -0,0 +1,50 @@
+import 'package:equatable/equatable.dart';
+
+class LocalUser extends Equatable {
+  const LocalUser({
+    required this.uid,
+    required this.email,
+    required this.points,
+    required this.fullName,
+    this.groupIds = const [],
+    this.enrolledCourseIds = const [],
+    this.following = const [],
+    this.followers = const [],
+    this.profilePic,
+    this.bio,
+  });
+
+  const LocalUser.empty()
+      : this(
+          uid: '',
+          email: '',
+          points: 0,
+          fullName: '',
+          profilePic: '',
+          bio: '',
+          groupIds: const [],
+          enrolledCourseIds: const [],
+          followers: const [],
+          following: const [],
+        );
+
+  final String uid;
+  final String email;
+  final String? profilePic;
+  final String? bio;
+  final int points;
+  final String fullName;
+  final List<String> groupIds;
+  final List<String> enrolledCourseIds;
+  final List<String> following;
+  final List<String> followers;
+
+  @override
+  List<Object?> get props => [uid, email];
+
+  @override
+  String toString() {
+    return 'LocalUser{uid: $uid, email: $email, bio: '
+        '$bio, points: $points, fullName: $fullName}';
+  }
+}
