3450465f0762962e7a54b1dacca4f7f98b8ec751
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-07-02 14:53:49 +0700

    Feat(auth): Finish auth firebase data source. All tests PASSED

--- lib/src/auth/data/datasources/auth.firebase.data-source.dart
+++ lib/src/auth/data/datasources/auth.firebase.data-source.dart
@@ -1,12 +1,18 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:education_app/core/enums/update-user.enum.dart';
+import 'package:education_app/core/errors/exceptions.dart';
+import 'package:education_app/core/utils/constants.dart';
+import 'package:education_app/core/utils/typedefs.dart';
 import 'package:education_app/src/auth/data/datasources/auth.data-source.dart';
 import 'package:education_app/src/auth/data/models/user.model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
 
-
-class FirebaseDataSource implements AuthDataSource{
+class FirebaseDataSource implements AuthDataSource {
   const FirebaseDataSource({
     required FirebaseAuth authClient,
     required FirebaseFirestore cloudStoreClient,
@@ -19,27 +25,169 @@ class FirebaseDataSource implements AuthDataSource{
   final FirebaseFirestore _cloudStoreClient;
   final FirebaseStorage _dbClient;
   @override
-  Future<void> forgotPassword(String email) {
-    // TODO: implement forgotPassword
-    throw UnimplementedError();
+  Future<void> forgotPassword(String email) async {
+    try {
+      await _authClient.sendPasswordResetEmail(email: email);
+    } on FirebaseAuthException catch (e) {
+      throw ServerException(
+        message: e.message ?? 'Error Occurred',
+        statusCode: e.code,
+      );
+    } catch (e, s) {
+      debugPrintStack(stackTrace: s);
+      throw ServerException(
+        message: e.toString(),
+        statusCode: '505',
+      );
+    }
   }
 
   @override
-  Future<LocalUserModel> signIn({required String email, required String password}) {
-    // TODO: implement signIn
-    throw UnimplementedError();
+  Future<LocalUserModel> signIn(
+      {required String email, required String password}) async {
+    try {
+      final result = await _authClient.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      final user = result.user;
+
+      if (user == null) {
+        throw const ServerException(
+          message: 'Please try again later',
+          statusCode: 'Unknown Error',
+        );
+      }
+      var userData = await _getUserData(user.uid);
+
+      if (userData.exists) {
+        return LocalUserModel.fromMap(userData.data()!);
+      }
+
+      // upload the user
+      await _setUserData(user, email);
+
+      userData = await _getUserData(user.uid);
+      return LocalUserModel.fromMap(userData.data()!);
+    } on FirebaseAuthException catch (e) {
+      throw ServerException(
+        message: e.message ?? 'Error Occurred',
+        statusCode: e.code,
+      );
+    } on ServerException {
+      rethrow;
+    } catch (e, s) {
+      debugPrintStack(stackTrace: s);
+      throw ServerException(
+        message: e.toString(),
+        statusCode: '505',
+      );
+    }
   }
 
   @override
-  Future<void> signUp({required String email, required String fullName, required String password}) {
-    // TODO: implement signUp
-    throw UnimplementedError();
+  Future<void> signUp(
+      {required String email,
+      required String fullName,
+      required String password}) async {
+    try {
+      final userCred = await _authClient.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      await userCred.user?.updateDisplayName(fullName);
+      await userCred.user?.updatePhotoURL(kDefaultAvatar);
+      await _setUserData(_authClient.currentUser!, email);
+    } on FirebaseAuthException catch (e) {
+      throw ServerException(
+        message: e.message ?? 'Error Occurred',
+        statusCode: e.code,
+      );
+    } catch (e, s) {
+      debugPrintStack(stackTrace: s);
+      throw ServerException(
+        message: e.toString(),
+        statusCode: '505',
+      );
+    }
   }
 
   @override
-  Future<void> updateUser({required EUpdateUserAction action, required userData}) {
-    // TODO: implement updateUser
-    throw UnimplementedError();
+  Future<void> updateUser(
+      {required EUpdateUserAction action, required userData}) async {
+    try {
+      switch (action) {
+        case EUpdateUserAction.email:
+          await _authClient.currentUser?.updateEmail(userData as String);
+          await _updateUserData({'email': userData});
+        case EUpdateUserAction.displayName:
+          await _authClient.currentUser?.updateDisplayName(userData as String);
+          await _updateUserData({'fullName': userData});
+        case EUpdateUserAction.profilePic:
+          final ref = _dbClient
+              .ref()
+              .child('profile_pics/${_authClient.currentUser?.uid}');
+
+          await ref.putFile(userData as File);
+          final url = await ref.getDownloadURL();
+          await _authClient.currentUser?.updatePhotoURL(url);
+          await _updateUserData({'profilePic': url});
+        case EUpdateUserAction.password:
+          if (_authClient.currentUser?.email == null) {
+            throw const ServerException(
+              message: 'User does not exist',
+              statusCode: 'Insufficient Permission',
+            );
+          }
+          final newData = jsonDecode(userData as String) as DataMap;
+          await _authClient.currentUser?.reauthenticateWithCredential(
+            EmailAuthProvider.credential(
+              email: _authClient.currentUser!.email!,
+              password: newData['oldPassword'] as String,
+            ),
+          );
+          await _authClient.currentUser?.updatePassword(
+            newData['newPassword'] as String,
+          );
+        case EUpdateUserAction.bio:
+          await _updateUserData({'bio': userData as String});
+      }
+    } on FirebaseException catch (e) {
+      throw ServerException(
+        message: e.message ?? 'Error Occurred',
+        statusCode: e.code,
+      );
+    } catch (e, s) {
+      debugPrintStack(stackTrace: s);
+      throw ServerException(
+        message: e.toString(),
+        statusCode: '505',
+      );
+    }
   }
-  
+
+  Future<DocumentSnapshot<DataMap>> _getUserData(String uid) async {
+    return _cloudStoreClient.collection('users').doc(uid).get();
+  }
+
+  Future<void> _setUserData(User user, String fallbackEmail) async {
+    await _cloudStoreClient.collection('users').doc(user.uid).set(
+          LocalUserModel(
+            uid: user.uid,
+            email: user.email ?? fallbackEmail,
+            fullName: user.displayName ?? '',
+            profilePic: user.photoURL ?? '',
+            points: 0,
+          ).toMap(),
+        );
+  }
+
+  Future<void> _updateUserData(DataMap data) async {
+    await _cloudStoreClient
+        .collection('users')
+        .doc(_authClient.currentUser?.uid)
+        .update(data);
+  }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_storage
+import google_sign_in_ios
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
@@ -25,6 +26,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
