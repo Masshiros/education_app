a095b51eed83c6ea074d9d0db86782dfc0bcccaa
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-07-03 12:04:38 +0700

    Feat(auth): Finish Auth BLOC

--- lib/src/auth/presentation/bloc/bloc/auth-bloc.dart
+++ lib/src/auth/presentation/bloc/bloc/auth-bloc.dart
@@ -23,9 +23,57 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
         _signUpUseCase = signUpUseCase,
         _forgotPasswordUseCase = forgotPasswordUseCase,
         _updateUserUseCase = updateUserUseCase,
-        super(const AuthInitial()) {}
+        super(const AuthInitial()) {
+    on<AuthEvent>((event, emit) {
+      emit(const AuthLoading());
+    });
+    on<SignInEvent>(_signInHandler);
+    on<SignUpEvent>(_signUpHandler);
+    on<ForgotPassWordEvent>(_forgotPasswordHandler);
+    on<UpdateUserEvent>(_updateUserHandler);
+  }
   final SignInUseCase _signInUseCase;
   final SignUpUseCase _signUpUseCase;
   final ForgotPasswordUseCase _forgotPasswordUseCase;
   final UpdateUserUseCase _updateUserUseCase;
+  Future<void> _signInHandler(
+      SignInEvent event, Emitter<AuthState> emit) async {
+    final result = await _signInUseCase(
+        SignInParams(email: event.email, password: event.password));
+    result.fold(
+      (f) => emit(AuthError(f.errorMessage)),
+      (user) => emit(SignedIn(user)),
+    );
+  }
+
+  Future<void> _signUpHandler(
+      SignUpEvent event, Emitter<AuthState> emit) async {
+    final result = await _signUpUseCase(SignUpParams(
+        email: event.email,
+        password: event.password,
+        fullName: event.fullName));
+    result.fold(
+      (f) => emit(AuthError(f.errorMessage)),
+      (_) => emit(const SignedUp()),
+    );
+  }
+
+  Future<void> _forgotPasswordHandler(
+      ForgotPassWordEvent event, Emitter<AuthState> emit) async {
+    final result = await _forgotPasswordUseCase(event.email);
+    result.fold(
+      (f) => emit(AuthError(f.errorMessage)),
+      (_) => emit(const ForgotPasswordSent()),
+    );
+  }
+
+  Future<void> _updateUserHandler(
+      UpdateUserEvent event, Emitter<AuthState> emit) async {
+    final result = await _updateUserUseCase(
+        UpdateUserParams(action: event.action, userData: event.userData));
+    result.fold(
+      (f) => emit(AuthError(f.errorMessage)),
+      (_) => emit(const UserUpdated()),
+    );
+  }
 }
