176d85a4529218e2050db9005b9a9382a3655479
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-07-07 16:15:31 +0700

    Feat(course): Finish data source implementation. All tests PASSED

--- lib/src/course/data/datasources/course.firebase.data-source.dart
+++ lib/src/course/data/datasources/course.firebase.data-source.dart
@@ -1,10 +1,15 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:education_app/core/errors/exceptions.dart';
+import 'package:education_app/src/chat/data/models/group.models.dart';
 import 'package:education_app/src/course/data/datasources/course.data-source.dart';
+import 'package:education_app/src/course/data/models/course.model.dart';
 import 'package:education_app/src/course/domain/entities/course.entities.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
-class CourseFirebaseDataSource implements CourseDataSource{
+class CourseFirebaseDataSource implements CourseDataSource {
   const CourseFirebaseDataSource({
     required FirebaseAuth authClient,
     required FirebaseFirestore cloudStoreClient,
@@ -17,15 +22,82 @@ class CourseFirebaseDataSource implements CourseDataSource{
   final FirebaseFirestore _cloudStoreClient;
   final FirebaseStorage _dbClient;
   @override
-  Future<void> addCourse(Course course) {
-    // TODO: implement addCourse
-    throw UnimplementedError();
+  Future<void> addCourse(Course course) async {
+    try {
+      final user = _authClient.currentUser;
+      if (user == null) {
+        throw const ServerException(
+          message: 'User is not authenticated',
+          statusCode: '401',
+        );
+      }
+      final courseRef = _cloudStoreClient.collection('courses').doc();
+      final groupRef = _cloudStoreClient.collection('groups').doc();
+
+      var courseModel = (course as CourseModel).copyWith(
+        id: courseRef.id,
+        groupId: groupRef.id,
+      );
+
+      if (courseModel.imageIsFile) {
+        final imageRef = _dbClient.ref().child(
+              'courses/${courseModel.id}/profile_image'
+              '/${courseModel.title}-pfp',
+            );
+
+        await imageRef.putFile(File(courseModel.image!)).then((value) async {
+          final url = await value.ref.getDownloadURL();
+          courseModel = courseModel.copyWith(image: url);
+        });
+      }
+
+      await courseRef.set(courseModel.toMap());
+
+      final group = GroupModel(
+        id: groupRef.id,
+        name: course.title,
+        members: const [],
+        courseId: courseRef.id,
+        groupImageUrl: courseModel.image,
+      );
+
+      return groupRef.set(group.toMap());
+    } on FirebaseException catch (e) {
+      throw ServerException(
+        message: e.message ?? 'Unknown error occurred',
+        statusCode: e.code,
+      );
+    } on ServerException {
+      rethrow;
+    } catch (e) {
+      throw ServerException(message: e.toString(), statusCode: '505');
+    }
   }
 
   @override
   Future<List<Course>> getCourses() {
-    // TODO: implement getCourses
-    throw UnimplementedError();
+    try {
+      final user = _authClient.currentUser;
+      if (user == null) {
+        throw const ServerException(
+          message: 'User is not authenticated',
+          statusCode: '401',
+        );
+      }
+      return _cloudStoreClient.collection('courses').get().then(
+            (value) => value.docs
+                .map((doc) => CourseModel.fromMap(doc.data()))
+                .toList(),
+          );
+    } on FirebaseException catch (e) {
+      throw ServerException(
+        message: e.message ?? 'Unknown error occurred',
+        statusCode: e.code,
+      );
+    } on ServerException {
+      rethrow;
+    } catch (e) {
+      throw ServerException(message: e.toString(), statusCode: '505');
+    }
   }
-  
 }
