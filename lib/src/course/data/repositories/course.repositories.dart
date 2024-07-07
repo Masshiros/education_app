69e71b4cb04b32b364fed386e4cc44175abcb87b
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-07-07 12:41:47 +0700

    Feat(course): Add course repositories implementation. All tests PASSED

--- lib/src/course/data/repositories/course.repositories.dart
+++ lib/src/course/data/repositories/course.repositories.dart
@@ -1,3 +1,6 @@
+import 'package:dartz/dartz.dart';
+import 'package:education_app/core/errors/exceptions.dart';
+import 'package:education_app/core/errors/failures.dart';
 import 'package:education_app/core/utils/typedefs.dart';
 import 'package:education_app/src/course/data/datasources/course.data-source.dart';
 import 'package:education_app/src/course/domain/entities/course.entities.dart';
@@ -7,14 +10,22 @@ class CourseRepository implements ICourseRepository {
   const CourseRepository(this._dataSource);
   final CourseDataSource _dataSource;
   @override
-  ResultFuture<void> addCourse(Course course) {
-    // TODO: implement addCourse
-    throw UnimplementedError();
+  ResultFuture<void> addCourse(Course course) async {
+    try {
+      await _dataSource.addCourse(course);
+      return Right(null);
+    } on ServerException catch (e) {
+      return Left(ServerFailure(message: e.message, statusCode: e.statusCode));
+    }
   }
 
   @override
-  ResultFuture<List<Course>> getCourses() {
-    // TODO: implement getCourses
-    throw UnimplementedError();
+  ResultFuture<List<Course>> getCourses() async {
+    try {
+      final result = await _dataSource.getCourses();
+      return Right(result);
+    } on ServerException catch (e) {
+      return Left(ServerFailure(message: e.message, statusCode: e.statusCode));
+    }
   }
 }
