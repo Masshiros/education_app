a70bca13d87a90d8b468f04cbdf0f7ada3827b1d
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-07-12 11:27:31 +0700

    Feat(video): Add video repository implementation. All tests PASSED

--- lib/src/course/features/videos/data/repositories/video.repositories.dart
+++ lib/src/course/features/videos/data/repositories/video.repositories.dart
@@ -1,3 +1,6 @@
+import 'package:dartz/dartz.dart';
+import 'package:education_app/core/errors/exceptions.dart';
+import 'package:education_app/core/errors/failures.dart';
 import 'package:education_app/core/utils/typedefs.dart';
 import 'package:education_app/src/course/features/videos/data/datasources/video.data-source.dart';
 import 'package:education_app/src/course/features/videos/domain/entities/video.entities.dart';
@@ -7,14 +10,21 @@ class VideoRepository implements IVideoRepository {
   const VideoRepository(this._dataSource);
   final VideoDataSource _dataSource;
   @override
-  ResultFuture<void> addVideo(Video video) {
-    // TODO: implement addVideo
-    throw UnimplementedError();
+  ResultFuture<void> addVideo(Video video) async {
+    try {
+      await _dataSource.addVideo(video);
+      return Right(null);
+    } on ServerException catch (e) {
+      return Left(ServerFailure(message: e.message, statusCode: e.statusCode));
+    }
   }
 
   @override
-  ResultFuture<List<Video>> getVideos(String courseId) {
-    // TODO: implement getVideos
-    throw UnimplementedError();
+  ResultFuture<List<Video>> getVideos(String courseId) async {
+    try {
+      return Right(await _dataSource.getVideos(courseId));
+    } on ServerException catch (e) {
+      return Left(ServerFailure(message: e.message, statusCode: e.statusCode));
+    }
   }
 }
